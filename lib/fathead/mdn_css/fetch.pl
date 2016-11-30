14f93912ec05396d1a4ec54ef350818d04bf5790
Harrison Chienjo <hhathersage@gmail.com>
2016-11-30 13:02:03 +0300

    Perltidy the code.

--- lib/fathead/mdn_css/fetch.pl
+++ lib/fathead/mdn_css/fetch.pl
@@ -43,8 +43,9 @@ queue_urls_for_download();
 Mojo::IOLoop->recurring(
     0 => sub {
         for ( $current_active_connections + 1 .. $maximum_active_connections ) {
-            return ( $current_active_connections or Mojo::IOLoop->stop_gracefully )
-                unless my $url = shift @keyword_urls;
+            return ( $current_active_connections
+                  or Mojo::IOLoop->stop_gracefully )
+              unless my $url = shift @keyword_urls;
 
             ++$current_active_connections;
             $ua->get(
@@ -58,10 +59,13 @@ Mojo::IOLoop->recurring(
                           "$file_number.html";
                         ++$file_number;
 
-                        #if the keyword URL redirects to another URL, write the keywords to a file
-                        if($url ne $tx->req->url) {
-                            my $keyword  = substr ($url, rindex($url, "/") + 1);
-                            my $redirect = substr ($tx->req->url, rindex($tx->req->url, "/") + 1);
+      #if the keyword URL redirects to another URL, write the keywords to a file
+                        if ( $url ne $tx->req->url ) {
+                            my $keyword =
+                              substr( $url, rindex( $url, "/" ) + 1 );
+                            my $redirect =
+                              substr( $tx->req->url,
+                                rindex( $tx->req->url, "/" ) + 1 );
                             $redirect_map{$redirect} = $keyword;
                         }
                     }
@@ -82,7 +86,7 @@ Mojo::IOLoop->recurring(
 Mojo::IOLoop->start unless Mojo::IOLoop->is_running;
 
 # write collected redirects to the file
-DumpFile('redirect_urls.yml', { redirects => {%redirect_map} } );
+DumpFile( 'redirect_urls.yml', { redirects => {%redirect_map} } );
 
 sub process_transaction {
     my $tx = shift;
